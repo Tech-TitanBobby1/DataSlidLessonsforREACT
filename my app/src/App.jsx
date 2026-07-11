aea5107a47fa327f5a25f37b7cdb26eff9b70c12
Bobby <techtitanbobby@gmail.com>
2026-07-11 10:36:08 +0100

    add react app files

---
+++ my app/src/App.jsx
@@ -0,0 +1,13 @@
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <h1>Hello World!</h1>
+      <p> I am here to learn React!</p>
+    </div>
+  )
+}
+
+export default App
